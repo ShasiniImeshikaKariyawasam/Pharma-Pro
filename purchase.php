caabc1b9695891849ba664e5d4e410f50647dc0c
ShasiniImeshikaKariyawasam <36620375+ShasiniImeshikaKariyawasam@users.noreply.github.com>
2020-09-11 22:05:08 +0530

    Add files via upload

---
+++ purchase.php
@@ -0,0 +1,42 @@
+<?php include('include/connection.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Purchase Order</title>
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container" style="margin-top: 100px;box-shadow: 0 0 15px 0 lightgrey;padding: 10px 10px;">
+	<div class="alert alert-info" role="alert" style="text-align: center;">
+		<h2>Purchase Order Generation</h2>
+	</div>
+	<div class="col-md-6" style="margin-left: 300px;">
+		<form method="post" action="purchaseOrder.php">
+			<label>P/O No: </label>
+			<input type="number" name="invoice" id="invoice" class="form-control">
+			<label>Date</label>
+			<?php $today = date('Y-m-d'); ?>
+			<input type="date" name="date" id="date" value = "<?php echo $today;?>" class="form-control" readonly>
+			<label>Supplier</label>
+			<select class="form-control" name="supplier">
+				<?php 
+					$supplier = "SELECT * FROM supplier";
+					$result = mysqli_query($con,$supplier);
+					while($row = mysqli_fetch_assoc($result)){
+						$supplier_id = $row['supplier_id'];
+						$supplier_name = $row['supplier_name'];
+						echo '<option value='.$supplier_id.'>'.$supplier_name.'</option>';
+					}
+				?>
+			</select>
+			<label>No of items</label>
+			<input type="number" name="item" id="item" class="form-control">
+			<div style="margin-top: 20px;">
+				<input type="submit" name="generate" id="generate" onclick='generate()' value="Generate" class="btn btn-info" style="margin-left: 100px;width: 100px;">
+				<input type="reset" name="reset" value="reset" class="btn btn-danger" style="margin-left: 100px;width: 100px;">
+			</div>
+		</form>
+	</div>
+</div>
+</body>
+</html>
